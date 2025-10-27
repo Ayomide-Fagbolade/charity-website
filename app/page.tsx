b3e156841e4f8ee4cac88cb7b40372be82b7dede
Ayomide-Fagbolade <ayomide.fagbolade@um6p.ma>
2025-10-27 13:31:06 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Page() {
                 who need it most.
               </p>
               <div className="flex flex-wrap gap-4 pt-4">
-                <Button size="lg" variant="green"  asChild>
+                <Button size="lg" className="bg-fuchsia-200" asChild>
                   <Link href="/donate">
                     Donate Now <Heart className="ml-2 h-4 w-4" />
                   </Link>
